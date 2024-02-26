a0cc03f230c6af9c763c4c838232254c2b07e510
AlexjrFS <afortunatosacramento@gmail.com>
2024-02-26 09:22:42 -0300

    .

--- gerar_certificados.js
+++ gerar_certificados.js
@@ -4,26 +4,68 @@ const pptxgen = require('pptxgenjs');
 const moment = require('moment');
 
 
+
 // Ler os dados do Excel
-const workbook = xlsx.readFile('dados_certificados.xlsx');
+const workbook = xlsx.readFile('dados_certificados.xlsx', { cellText: false });
 const sheet = workbook.Sheets[workbook.SheetNames[0]];
 const data = xlsx.utils.sheet_to_json(sheet);
 
+const colunaCPF = sheet['B'];
+
+// Inicializar um array para armazenar os CPFs
+const cpfs = [];
+
+// Iterar sobre as células da coluna "CPF" e extrair os valores
+for (const celula in colunaCPF) {
+    if (celula.startsWith('B') && colunaCPF.hasOwnProperty(celula)) {
+        const valorCPF = colunaCPF[celula].v;
+        // Adicionar o CPF ao array
+        cpfs.push(valorCPF);
+    }
+}
+
+function formatarCPF(cpf) {
+    // Verificar se o CPF é uma string
+    if (typeof cpf !== 'string') {
+        // Se não for uma string, retornar o CPF original
+        return cpf;
+    }
+
+    // Limpar CPF
+    const cpfLimpo = cpf.replace(/\D/g, '');
+
+    // Adicionar pontos e traço
+    const cpfFormatado = cpfLimpo.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+
+    return cpfFormatado;
+}
+
+
+// Iterar sobre os CPFs e formatá-los
+const cpfsFormatados = cpfs.map(formatarCPF);
+
+// Agora, `cpfsFormatados` contém os CPFs formatados
+console.log(cpfsFormatados);
+
 // Modelo de texto do certificado
-const modeloCertificado = "Certificamos para todos os fins que {NOME_DO_PARTICIPANTE}, portador(a) do CPF {CPF_PARTICIPANTE}, concluiu o curso {NOME_CURSO}, realizado no dia {DATA_CURSO}, com carga horária de {CARGA_HORARIA} horas e conteúdo programático relacionado no verso, promovido nas dependências da empresa {NOME_EMPRESA}, {ENDERECO_EMPRESA}.";
+const modeloCertificado = "Certificamos para todos os fins que {NOME_DO_PARTICIPANTE}, portador(a) do CPF: {CPF_PARTICIPANTE}, concluiu o curso {NOME_CURSO}, realizado no dia {DATA_CURSO}, com carga horária de {CARGA_HORARIA} horas e conteúdo programático relacionado no verso, promovido nas dependências da empresa {NOME_EMPRESA}, {ENDERECO_EMPRESA}.";
 
 // Iterar sobre os dados e criar certificados em PowerPoint
 data.forEach(row => {
     const nomeParticipante = row['Nome'];
-    const cpfParticipante = row['CPF'];
+    const cpfParticipante = typeof row['CPF'] === 'string' ? formatarCPF(row['CPF'].trim()) : row['CPF'];
     const nomeCurso = row['Nome do Curso'];
-    const dataCurso = row['Data do Curso'] instanceof Date
-        ? moment(row['Data do Curso']).format('DD/MM/YYYY')
-        : row['Data do Curso'] ? xlsx.SSF.parse_date_code(row['Data do Curso']) : '';
     const cargaHoraria = row['Carga Horária'];
     const nomeEmpresa = row['Nome da Empresa'];
     const enderecoEmpresa = row['Endereço da Empresa'];
+    const cpfParticipanteFormatado = formatarCPF(cpfParticipante);
+    const dataCursoExcel = row['Data do Curso'];
 
+    // Converter o número serial do Excel para data
+    const dataCursoNumerica = parseFloat(dataCursoExcel);
+    const dataCursoFormatada = !isNaN(dataCursoNumerica)
+        ? moment("1900-01-01").add(dataCursoNumerica, 'days').format('DD/MM/YYYY')
+        : 'data não informada';
     // Criar uma apresentação em branco
     const presentation = new pptxgen();
 
@@ -41,9 +83,9 @@ data.forEach(row => {
     // Substituir variáveis no modelo de certificado
     const textoCertificado = modeloCertificado
         .replace('{NOME_DO_PARTICIPANTE}', nomeParticipante)
-        .replace('{CPF_PARTICIPANTE}', cpfParticipante)
+        .replace('{CPF_PARTICIPANTE}', cpfParticipanteFormatado)
         .replace('{NOME_CURSO}', nomeCurso)
-        .replace('{DATA_CURSO}', dataCurso)
+        .replace('{DATA_CURSO}', dataCursoFormatada)
         .replace('{CARGA_HORARIA}', cargaHoraria)
         .replace('{NOME_EMPRESA}', nomeEmpresa)
         .replace('{ENDERECO_EMPRESA}', enderecoEmpresa);
